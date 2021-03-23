f8098cb35f43216112d40cdb31904089cde944ba
tysong24 <tommysong123@gmail.com>
2021-03-23 15:18:06 -0700

    inital screen created

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [people, setPeople] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Welcome to Splitty!</Text>
+      <TextInput
+        onChangeText={(text) => {
+          if (typeof text === 'number') {
+            setPeople(text);
+          }
+        }}
+        placeholder='Enter # of people'
+        keyboardType='numeric'
+        style={styles.input}
+      />
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -17,5 +29,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    color: 'darkgrey',
+    fontWeight: 'bold',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 0,
+    textAlignVertical: 'center',
   },
 });
